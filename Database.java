a91f249643a5a092d45be37a63c59e5112512a93
Greg <gharms@uark.edu>
2016-06-24 19:56:50 -0500

    added methods for setting and retrieving data from sql

--- Database.java
+++ Database.java
@@ -5,7 +5,7 @@ import java.io.*;
 abstract class Database {
 	static final String url = "jdbc:mysql://localhost:3306/kayaktivity";
 	static final String username = "greg";
-	static final String password = "(Critical)<Jack>";
+	static final String password = "<notrealpassword>";
 	
 	Database() {
 	
@@ -75,6 +75,166 @@ abstract class Database {
 	}
 	
 	
+	// add riverToAdd to the database. returns 1 if it worked, -1 if failed.
+	public static String addRiver(Connection conn, String riverToAdd) {
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		// increment ID's in database
+		int lastId = getLastId(conn);
+		if (lastId == -1) 
+			return "-1";
+		lastId = lastId + 1;
+		
+		String sqlCommand = "INSERT INTO rivers (id,name) VALUES (" + lastId + ", '" + riverToAdd + "')"; 
+		
+		try {
+			stmt = conn.createStatement();
+			stmt.execute(sqlCommand);
+			return "1";
+		}
+		catch (SQLException ex){
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+		}
+		finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException sqlEx) { } // ignore
+
+				rs = null;
+			}
+
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException sqlEx) { } // ignore
+
+				stmt = null;
+			}
+		}
+		return "-1";
+	}
+	
+	//get sections by river. returns list of access points in JSON or -1 if it fails
+	String getAccesses(Connection conn, String river) {
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		String sqlCommand = "SELECT * FROM rivers WHERE name = '" + river + "'"; 
+		
+		try {
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sqlCommand);
+			rs.next();
+			return rs.getString("access");
+		}
+		catch (SQLException ex){
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+		}
+		finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException sqlEx) { } // ignore
+
+				rs = null;
+			}
+
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException sqlEx) { } // ignore
+
+				stmt = null;
+			}
+		}
+		System.out.println("getAccesses failed");
+		return "-1";
+	}
+	
+	// add an access point to a river. takes JSON input {"river": "<river_name>", "access": "<access_name>"}
+	// returns 1 if it works, -1 if it doesn't, "section exists" if it's already added
+	String addAccess(Connection conn, String toSQL) {
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		//parse json
+		String river = null;
+		String access = null;
+		if (toSQL.contains("river\": ")) {
+			int beginning = toSQL.indexOf("river\": ");
+			beginning += 9;
+			int end = toSQL.indexOf(',', beginning) - 1;
+			river = toSQL.substring(beginning, end);
+		}
+		else {
+			System.out.println("invalid input to addAccess");
+			return "-1";
+		}
+		if (toSQL.contains("access\": ")) {
+			int beginning = toSQL.indexOf("access\": ");
+			beginning += 10;
+			int end = toSQL.indexOf('"', beginning);
+			access = toSQL.substring(beginning, end);
+		}
+		else {
+			System.out.println("invalid input to addAccess");
+			return "-1";
+		}
+		
+		String newAccessList = null;
+		//check if access already exists and append entry
+		String accessList = getAccesses(conn, river);
+		if (accessList != null) {
+			if (accessList.contains(access))
+				return "access exists";
+			
+			int indexInsert = accessList.indexOf('}');
+			newAccessList = accessList.substring(0, indexInsert) + ", access: " + access + "}";
+		}
+		else 
+			newAccessList = "{access: " + access + "}";
+			
+		// make SQL command
+		String sqlCommand = "UPDATE rivers SET access=\"" + newAccessList + "\" WHERE name=\"" + river + "\""; 
+		
+		try {
+			stmt = conn.createStatement();
+			stmt.execute(sqlCommand);
+			return "1";
+		}
+		catch (SQLException ex){
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+		}
+		finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException sqlEx) { } // ignore
+
+				rs = null;
+			}
+
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException sqlEx) { } // ignore
+
+				stmt = null;
+			}
+		}
+		return "-1";
+	}
 // 	boolean addSection(int id, String river, String putIn, String takeOut, int distance, int optLevel, String guage, 
 // 											int rapidClass, String description) {
 // 		Connection conn = getConnecton();
--- DbAddRiver.java
+++ DbAddRiver.java
@@ -4,50 +4,11 @@ import java.io.*;
 
 class DbAddRiver extends Database{
 
-	String add(String toSQL) {
-		return getConnection(toSQL);
+	String add(String riverToAdd) {
+		return getConnection(riverToAdd);
 	}
 
-	String doIt(Connection conn, String toSQL) {
-		Statement stmt = null;
-		ResultSet rs = null;
-		
-		int lastId = getLastId(conn);
-		if (lastId == -1) 
-			return "-1";
-			
-		lastId = lastId + 1;
-		
-		String sqlCommand = "INSERT INTO rivers VALUES (" + lastId + ", \"Mulberry\", \"Turner's Bend\")"; 
-		
-		try {
-			stmt = conn.createStatement();
-			stmt.execute(sqlCommand);
-			return "0";
-		}
-		catch (SQLException ex){
-			// handle any errors
-			System.out.println("SQLException: " + ex.getMessage());
-			System.out.println("SQLState: " + ex.getSQLState());
-			System.out.println("VendorError: " + ex.getErrorCode());
-		}
-		finally {
-			if (rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException sqlEx) { } // ignore
-
-				rs = null;
-			}
-
-			if (stmt != null) {
-				try {
-					stmt.close();
-				} catch (SQLException sqlEx) { } // ignore
-
-				stmt = null;
-			}
-		}
-		return "-1";
+	String doIt(Connection conn, String riverToAdd) {
+		return addRiver(conn, riverToAdd);
 	}
 }
